da59ac515a787d79c6c6493d27120a0b8eb40430
tarekyou <89763835+tarekyou@users.noreply.github.com>
2022-01-25 22:41:23 -0800

    footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import About from "./components/About";
 import ContactForm from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <About />
       <ContactForm></ContactForm>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>
+        <a href="https://github.com/tarekyou" target="_blank">
+          github
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/user" target="_blank">
+          linkedin
+        </a>
+      </div>
+      <div>
+        <a href="https://stackoverflow.com/user" target="_blank">
+          stackoverflow
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
