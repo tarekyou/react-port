0fbdc7b0974ac44bff9251f641b1b7c876966450
tarekyou <89763835+tarekyou@users.noreply.github.com>
2022-01-25 22:36:07 -0800

    contact component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import About from "./components/About";
+import ContactForm from "./components/Contact";
 
 function App() {
   return (
     <div>
       <About />
+      <ContactForm></ContactForm>
     </div>
   );
 }
