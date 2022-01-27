fc173ee5fe3f8241c5e37f2c6c9c8367a83109f2
tarekyou <89763835+tarekyou@users.noreply.github.com>
2022-01-27 13:16:06 -0800

    app js to render spa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState } from "react";
 import About from "./components/About";
 import ContactForm from "./components/Contact";
 import Footer from "./components/Footer";
@@ -7,16 +8,47 @@ import Resume from "./components/Resume";
 import Header from "./components/Header";
 
 function App() {
+  const [currentCategory, setCurrentCategory] = useState("about");
+
+  const displayCat = () => {
+    switch (currentCategory) {
+      case "about":
+        return <About />;
+      case "portfolio":
+        return <Portfolio />;
+      case "contact":
+        return <ContactForm />;
+      case "resume":
+        return <Resume />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div>
-      <Header></Header>
-      <About />
-      <Portfolio></Portfolio>
-      <ContactForm></ContactForm>
-      <Resume></Resume>
-      <Footer></Footer>
+      <div>
+        <Header
+          currentCategory={currentCategory}
+          setCurrentCategory={setCurrentCategory}
+        ></Header>
+      </div>
+      <div>
+        <div>{displayCat()}</div>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
   );
+  // <div>
+  //   <Header></Header>
+  //   <About />
+  //   <Portfolio></Portfolio>
+  //   <ContactForm></ContactForm>
+  //   <Resume></Resume>
+  //   <Footer></Footer>
+  // </div>
 }
 
 export default App;
