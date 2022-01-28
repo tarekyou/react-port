b06bc480223d86e361c3f81df174e79a2ee84da2
tarekyou <89763835+tarekyou@users.noreply.github.com>
2022-01-27 22:30:01 -0800

    portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,44 +2,45 @@ import React, { useState } from "react";
 import ProjectList from "../ProjectList";
 
 function Portfolio() {
-  const [projects] = useState([
+  //   const [projects] = useState([
+  const projects = [
     {
       title: "Budget tracker",
-      image: "../../assets/Budget-Tracker.png",
+      image: "Budget-Tracker.png",
       repo: "https://github.com/tarekyou/trackbudget",
       deployed: "https://boiling-scrubland-45534.herokuapp.com/",
     },
     {
       title: "Note taker",
-      image: "../../assets/Note-Taker.png",
+      image: "Note-Taker.png",
       repo: "https://github.com/tarekyou/note-taker",
       deployed: "https://protected-beach-90412.herokuapp.com/",
     },
     {
       title: "Password Generator",
-      image: "../../assets/Password-Generator.png",
+      image: "Password-Generator.png",
       repo: "https://github.com/tarekyou/password-generator",
       deployed: "https://tarekyou.github.io/password-generator/",
     },
     {
       title: "Timed Javascript quiz",
-      image: "../../assets/Timed-Javascript-Quiz.png",
+      image: "Timed-Javascript-Quiz.png",
       repo: "https://github.com/tarekyou/quiz-challenge",
       deployed: "https://tarekyou.github.io/quiz-challenge/",
     },
     {
       title: "Weather Dashboard",
-      image: "../../assets/Weather-Dashboard.png",
+      image: "Weather-Dashboard.png",
       repo: "https://github.com/tarekyou/weather-dash",
       deployed: "https://tarekyou.github.io/weather-dash/",
     },
     {
       title: "Work day scheduler",
-      image: "../../assets/Work-Day-Scheduler.png",
+      image: "Work-Day-Scheduler.png",
       repo: "https://github.com/tarekyou/workday-scheduler",
       deployed: "https://tarekyou.github.io/workday-scheduler/",
     },
-  ]);
+  ];
   return (
     <div>
       <ProjectList projects={projects} />
--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ProjectList(props) {
-  const { projects = [], title, image, deployed, repo } = props;
-  //   const projects = [];
-  //   const currentProject = useState(props)[0].projects;
+  const { projects = [] } = props;
   //   const { title, image, deployed, repo } = currentProject;
   return (
     <div>
       <ul>
         {projects.map((project) => (
           <li key={project.title}>
-            <h3>{title}</h3>
-            <img src={image}></img>
-            <a href={deployed}> deployed application</a>
-            <a href={repo}>repo</a>
+            <h3>{project.title}</h3>
+            <img
+              src={require(`../../assets/${project.image}`)}
+              //   src={project.image}
+              //   alt={image.name}
+              //   key={image.name}
+            ></img>
+            <a href={project.deployed} target="_blank">
+              {" "}
+              deployed application
+            </a>
+            <a href={project.repo} target="_blank">
+              repo
+            </a>
           </li>
         ))}
       </ul>
