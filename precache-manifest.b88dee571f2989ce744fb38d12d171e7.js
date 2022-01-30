f34471bddb09da916d91a4e367869763370f0b2e
tarekyou <89763835+tarekyou@users.noreply.github.com>
2022-01-29 16:23:10 -0800

    Updates

---
+++ precache-manifest.b88dee571f2989ce744fb38d12d171e7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7fcbcb7751166bf2958a12b00cded8",
+    "url": "/react-port/index.html"
+  },
+  {
+    "revision": "caa817c2cf29726e7f5e",
+    "url": "/react-port/static/css/main.966ee79e.chunk.css"
+  },
+  {
+    "revision": "592cf9483e0ee7c761d5",
+    "url": "/react-port/static/js/2.4cf17cbb.chunk.js"
+  },
+  {
+    "revision": "7b4b590765ba9cc9d7de",
+    "url": "/react-port/static/js/3.f1bbbdc7.chunk.js"
+  },
+  {
+    "revision": "caa817c2cf29726e7f5e",
+    "url": "/react-port/static/js/main.2fca03e4.chunk.js"
+  },
+  {
+    "revision": "b20b1c4121c7f183b1e0",
+    "url": "/react-port/static/js/runtime~main.6261ec37.js"
+  },
+  {
+    "revision": "d17ffd70622a65162293c76997b73425",
+    "url": "/react-port/static/media/Budget-Tracker.d17ffd70.png"
+  },
+  {
+    "revision": "689a2421a599056502df34744831cbf9",
+    "url": "/react-port/static/media/Note-Taker.689a2421.png"
+  },
+  {
+    "revision": "f4d09f37be07ba28fd1e171a5332705f",
+    "url": "/react-port/static/media/Password-Generator.f4d09f37.png"
+  },
+  {
+    "revision": "87398b5945b06593dc7332c8cd4aa82f",
+    "url": "/react-port/static/media/Timed-Javascript-Quiz.87398b59.png"
+  },
+  {
+    "revision": "b617369f6087954edec59813f52ae1d4",
+    "url": "/react-port/static/media/Weather-Dashboard.b617369f.png"
+  },
+  {
+    "revision": "2bbda24d3cdf8e04b225cd4694a5bb92",
+    "url": "/react-port/static/media/Work-Day-Scheduler.2bbda24d.png"
+  },
+  {
+    "revision": "139d34211ce9c9becf63ba462b8c5045",
+    "url": "/react-port/static/media/resume.139d3421.pdf"
+  }
+]);
